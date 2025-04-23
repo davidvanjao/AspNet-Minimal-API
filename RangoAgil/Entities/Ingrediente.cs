889f6ff6a77aeeb0e122cf52add235c2616fc3db
David Vanjao <davidrafaeldj@hotmail.com>
2025-04-23 04:23:29 -0300

    Add project files.

---
+++ RangoAgil/Entities/Ingrediente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RangoAgil.API.Entities;
+public class Ingrediente {
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public required string Nome { get; set; }
+    public ICollection<Rango> Rangos { get; set; } = new List<Rango>(); //tras todos os rangos que tem esse ingrediente. mesmo que colocar []
+    public Ingrediente() {
+           
+    }
+    [SetsRequiredMembers] //É um atributo que indica que um construtor (ou método) preenche todas as propriedades required de uma classe.
+    public Ingrediente(int id, string nome) {
+        Id = id;
+        Nome = nome;            
+    }
+}
+
---
+++ RangoAgil/Entities/Rango.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RangoAgil.API.Entities;
+public class Rango {
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public required string Nome { get; set; }
+    public ICollection<Ingrediente> Ingredientes { get; set; } = []; //tras todos os ingredientes que tem esse ingrediente. mesmo que colocar new List<Ingrediente>();
+    public Rango() {
+           
+    }
+    [SetsRequiredMembers]
+    public Rango(int id, string nome) {
+        Id = id;
+        Nome = nome;            
+    }
+}
+
